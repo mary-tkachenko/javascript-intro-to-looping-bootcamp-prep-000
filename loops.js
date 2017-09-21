dff6117ba6bf896883a04cb2ed394045b6b54d1b
Mary <l.skyling@gmail.com>
2017-09-21 14:31:29 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -16,3 +16,13 @@ function whileLoop(n) {
   }
   return "done";
 }
+
+function maybeTrue() {
+  array.push();
+}
+
+function doWhileLoop(array) {
+do {
+  return array;
+} while (array.length > 0 && maybeTrue(true));
+}
